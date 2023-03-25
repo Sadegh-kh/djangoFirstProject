80844b95594c4cf9ce45edc83d1300d70a85bef7
Sadegh <Sadeghkhoshbayan1@gmail.com>
2023-03-26 00:00:17 +0330

    register post model to admin panel and creat first post

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["title", "auther", "publish", "status"]
+    list_display_links = ['auther', 'title']
+    list_filter = ["publish", 'status', 'auther']
+    search_fields = ['title', 'description', 'auther']
+    list_editable = ['status']
+    prepopulated_fields = {'slug': ['auther', 'title']}
+    raw_id_fields = ('auther',)
+    date_hierarchy = 'publish'
+    ordering = ['-publish', 'title']
