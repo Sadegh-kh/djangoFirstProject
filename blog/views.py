4e12cbc7be992c77969938dadb8c2fe82b3163d4
Sadegh <Sadeghkhoshbayan1@gmail.com>
2023-04-13 16:54:13 +0330

    init blog views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def index_page(request):
+    return HttpResponse("index page")
+
+
+def post_list(request):
+    return HttpResponse("list of posts")
+
+
+def post_item(request, id):
+    return HttpResponse(f"post : {id}")
