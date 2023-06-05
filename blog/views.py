b484d67529dddecc69943400f2ce65a75a2bc5be
Sadegh <Sadeghkhoshbayan1@gmail.com>
2023-06-05 21:14:19 +0330

    init paginator on view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import Post
+from django.core.paginator import Paginator
 
 
 # Create your views here.
@@ -11,6 +12,9 @@ def index_page(request):
 
 def post_list(request):
     posts = Post.published.all()
+    paginator = Paginator(posts, 2)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
     context = {
         "list_of_post": posts
     }
