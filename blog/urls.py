ec226abcef3d3253b9ffebaa71c8e81ccda09fa6
Sadegh <Sadeghkhoshbayan1@gmail.com>
2023-06-14 22:01:56 +0330

    add ticket url

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", index_page, name="home"),
     path('posts/', PostList.as_view(), name="post_list"),
     path('posts/<pk>', PostItemDetail.as_view(), name="post_item"),
+    path("ticket/", ticket, name="blog-ticket")
 ]
