f6490b4839fc15ffd11df42b6f14c0bb5c95bb20
Sadegh <Sadeghkhoshbayan1@gmail.com>
2023-07-03 20:06:22 +0330

    init url comment

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ app_name = "blog"
 urlpatterns = [
     path("", index_page, name="home"),
     path('posts/', PostList.as_view(), name="post_list"),
-    path('posts/<pk>', PostItemDetail.as_view(), name="post_item"),
+    path('posts/<pk>', post_item, name="post_item"),
+    path('posts/<pk>/comment', post_comment, name="post_comment"),
     path("ticket/", ticket, name="blog-ticket")
 ]
