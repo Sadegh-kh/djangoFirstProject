9f33f96904d4efd187fa1e77b6e499001becab61
sadeg <Sadeghkhoshbayan1@gmail.com>
2023-07-17 13:26:20 +0330

    using serach query and search rank

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ from django.views.decorators.http import require_POST
 from django.db.models import Avg, Max, Min
 from django.contrib.auth.models import User
 from django.db.models import Q
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 
 # Create your views here.
@@ -129,7 +129,11 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title','description')).filter(search=query)
+            search_query = SearchQuery(query)
+            search_vector = SearchVector('title', weight="A") + SearchVector('description', weight="B")
+            rank = SearchRank(search_vector, search_query, weights=[0.1, 0.3, 0.6, 0.9])
+            results = Post.published.annotate(search=search_vector, rank=rank) \
+                .filter(rank__gte=0.4).order_by('-rank')
 
     context = {
         'query': query,
