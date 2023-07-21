36966042d0c8ceb304c82ce5ab41856c51a0ae7a
sadeg <Sadeghkhoshbayan1@gmail.com>
2023-07-21 10:19:02 +0330

    delete old image in os when delete or update from admin panel

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django_jalali.db import models as jmodels
 from django.urls import reverse
 from django_resized import ResizedImageField
+from django.db.models.signals import post_delete, pre_save
+from django.dispatch.dispatcher import receiver
 
 
 class PublishedManager(models.Manager):
@@ -104,3 +106,26 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title if self.title else "none"
+
+
+@receiver(post_delete, sender=Image)
+def delete_image(sender, instance, **kwargs):
+    """ delete old image from media root when deleted database """
+    instance.image_file.delete(save=False)
+
+
+@receiver(pre_save, sender=Image)
+def pre_save_image(sender, instance, *args, **kwargs):
+    """ instance old image file will delete from os """
+    try:
+        old_img = instance.__class__.objects.get(id=instance.id).image_file.path
+        try:
+            new_img = instance.image_file.path
+        except:
+            new_img = None
+        if new_img != old_img:
+            from os import path, remove
+            if path.exists(old_img):
+                remove(old_img)
+    except:
+        pass
